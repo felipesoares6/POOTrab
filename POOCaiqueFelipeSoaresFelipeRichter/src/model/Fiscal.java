715249db072804bcb0d3ab3130d4efe4ff0f47ef
felipesoares6 <felipeluizsoares@gmail.com>
2016-11-03 08:58:40 -0200

    Add class Fiscal

---
+++ POOCaiqueFelipeSoaresFelipeRichter/src/model/Fiscal.java
@@ -0,0 +1,28 @@
+package model;
+
+/**
+ *
+ * @author felipesoares
+ */
+public class Fiscal extends Pessoa {
+    private String codigo;
+    public String local;
+    
+    public Fiscal(String cpf, String nome, String endereco, String codigo) {
+        super(cpf, nome, endereco);
+        this.codigo = codigo;
+    }
+    
+    public void setLocal(String local){
+        this.local = local;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getLocal() {
+        return local;
+    }   
+    
+}
