b90588885fce5c729867630f1cc79b309e691d52
felipesoares6 <felipeluizsoares@gmail.com>
2016-11-03 08:52:54 -0200

    Add class pessoa

---
+++ POOCaiqueFelipeSoaresFelipeRichter/src/model/Pessoa.java
@@ -0,0 +1,50 @@
+package model;
+
+/**
+ *
+ * @author felipesoares
+ */
+public class Pessoa {
+    private String cpf, nome, endereco, telefone, email;
+    private Concurso concurso;
+    
+    public Pessoa (String cpf, String nome, String endereco){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+    
+    public Concurso getConcurso(){
+        return concurso;
+    }
+    
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+}
