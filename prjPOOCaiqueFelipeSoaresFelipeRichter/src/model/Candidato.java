898abace85e8b777f39b4b831af29876ecd1d44a
felipesoares6 <felipeluizsoares@gmail.com>
2016-11-29 12:07:16 -0200

    Fix class Candidato

--- prjPOOCaiqueFelipeSoaresFelipeRichter/src/model/Candidato.java
+++ prjPOOCaiqueFelipeSoaresFelipeRichter/src/model/Candidato.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author felipesoares
@@ -7,20 +9,20 @@ package model;
 public class Candidato extends Pessoa {
     private String inscricao;
     private double media;
-    private Prova[] provas;
+    private ArrayList<Prova> provas;
     
     public Candidato(String cpf, String nome, String endereco, String inscricao) {
         super(cpf, nome, endereco);
         this.inscricao = inscricao;
-        provas = new Prova[1];
+        provas = new ArrayList<Prova>();
     }
 
     public void calcularMedia() {
         double notas=0, pesos=0;
         
-        for(int i=0, qtdProvas = provas.length; i < qtdProvas; i++){
-             notas += provas[i].getPeso() * provas[i].getNota();
-             pesos += provas[i].getPeso();
+        for(Prova p: provas){
+             notas += p.getPeso() * p.getNota();
+             pesos += p.getPeso();
         }
         
         media = notas/pesos;
@@ -28,6 +30,10 @@ public class Candidato extends Pessoa {
     
     public double getMedia (){
         return media;
+    }
+    
+    public ArrayList<Prova> getProvas () {
+        return provas;
     }
     
    
