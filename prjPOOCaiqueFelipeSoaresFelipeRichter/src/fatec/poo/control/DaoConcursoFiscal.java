e8ae14b67828f7988777fca54ee4bf1a0b8a482b
Caique Mitsuoka <caique_mitsuoka@hotmail.com>
2016-12-12 08:02:37 -0200

    add consultar to DaoConcursoFiscal

--- prjPOOCaiqueFelipeSoaresFelipeRichter/src/fatec/poo/control/DaoConcursoFiscal.java
+++ prjPOOCaiqueFelipeSoaresFelipeRichter/src/fatec/poo/control/DaoConcursoFiscal.java
@@ -10,7 +10,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import model.Concurso;
+import model.Fiscal;
 
 /**
  *
@@ -68,5 +70,27 @@ public class DaoConcursoFiscal {
        } catch (SQLException ex) {
             System.out.println(ex.toString());   
        }
+    }
+    
+    public ArrayList<Fiscal> consultar (String sigla) {
+        ArrayList<Fiscal> f = new ArrayList();
+
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("SELECT * from tbConcurso_fiscal where " +
+                                       "sigla = ?");
+
+            ps.setString(1, sigla);
+            ResultSet rs = ps.executeQuery();
+
+            DaoFiscal daoFiscal = new DaoFiscal(this.conn);
+            while (rs.next()) {
+                f.add(daoFiscal.consultar(rs.getString("cpf")));
+            }
+        }
+        catch (SQLException ex) { 
+             System.out.println(ex.toString());   
+        }
+        return (f);
     }
 }
